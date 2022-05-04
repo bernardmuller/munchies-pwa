ead63a8da06b1a9fecd5192233cd11e9e2a03895
Bernard <b.mullerjnr@gmail.com>
2022-05-04 14:25:19 +0200

    space error fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import theme from 'common/theme';
+
 class MyDocument extends Document {
   render() {
     return (
